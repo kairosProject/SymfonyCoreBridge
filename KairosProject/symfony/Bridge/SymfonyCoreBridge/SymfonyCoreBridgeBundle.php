f5a57d39da5be06aebae1af6775831ca1b51b229
matthieu88160 <matthieu.vallance@cscfa.fr>
2018-08-25 15:44:48 +0200

    #15 initial commit

---
+++ KairosProject/symfony/Bridge/SymfonyCoreBridge/SymfonyCoreBridgeBundle.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is part of the kairos project.
+ *
+ * As each files provides by the CSCFA, this file is licensed
+ * under the MIT license.
+ *
+ * PHP version 7.2
+ *
+ * @category Symfony_Core_Bridge
+ * @package  Kairos_Project
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+namespace KairosProject\SymfonyCoreBridge;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Symfony core bridge bundle
+ *
+ * This class is used to offer the base configuration system of the kairos api.
+ *
+ * @category Symfony_Core_Bridge
+ * @package  Kairos_Project
+ * @author   matthieu vallance <matthieu.vallance@cscfa.fr>
+ * @license  MIT <https://opensource.org/licenses/MIT>
+ * @link     http://cscfa.fr
+ */
+class SymfonyCoreBridgeBundle extends Bundle
+{
+}
+
